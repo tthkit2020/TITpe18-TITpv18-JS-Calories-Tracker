7be783d7b5a0ca0751a4e774b7b7eb2bd838af17
Julia Aasmaa <my_e_10@hotmail.com>
2020-11-20 12:38:38 +0200

    commit

--- js/app.js
+++ js/app.js
@@ -2,6 +2,7 @@ const addButton = document.getElementById('add');
 const foodName = document.querySelector('#item-name');
 const foodCalories = document.querySelector('#item-calories');
 const totalSpan = document.querySelector('.total-calories');
+const list = document.querySelector('#item-list');
 let total = 0;
 
 
@@ -11,6 +12,14 @@ addButton.addEventListener('click', (event)=> {
     let mealCalories = parseInt(foodCalories.value); //get the calories from the input field
     total = total + mealCalories; //calculates the total calorie intake
     totalSpan.innerHTML = total;
+
+    let listItem = document.createElement('li');
+    listItem.classList.add('collection-item');
+    let text = `Food: ${mealName}. Calories: ${mealCalories}`;
+    listItem.appendChild(document.createTextNode(text));
+    console.log(listItem);
+    list.appendChild(listItem);
+
     //clear the input fields
     foodName.value = '';
     foodCalories.value = '';
